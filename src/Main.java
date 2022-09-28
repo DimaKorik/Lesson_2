4f9a9f5e48c20385ce9d641e45d7a529aa16aad4
DimaKorik <KorikDmitriy@mail.ru>
2022-09-28 21:20:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ещё чуть-чуть и прямо в рай");
+        System.out.println("Их жизнь удалась, what a beautiful life");
+        System.out.println("И все завидуют, пускай");
+        System.out.println("Ведь жить станет в кайф, what a beautiful life");
+    }
+}
